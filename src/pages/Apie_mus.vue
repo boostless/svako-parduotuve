cb9a659df3f30e2b644ed39aa53358bd81e8185a
boostless <jonas68824@gmail.com>
2023-01-03 20:55:56 +0200

    Add(pages): add routing pages

---
+++ src/pages/Apie_mus.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="w-full min-h-fit">
+        <h1 class="text-lg font-bold text-center">Apie mus</h1>
+    </div>
+</template>
---
+++ src/pages/Kontaktai.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="w-full min-h-fit">
+        <h1 class="text-lg font-bold text-center">Kontaktai</h1>
+    </div>
+</template>
