1362d0fc5957741fb4959ca00705242cf0021ddd
boostless <jonas68824@gmail.com>
2023-01-03 21:16:24 +0200

    Refactor(NavBar): use header and other stuff

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -26,15 +26,17 @@ export default {
 </script>
 
 <template>
-    <nav class="flex w-full h-48 bg-pink-200 shadow-md">
-        <div class="h-full w-2/4 mx-auto pt-8 pb-4 flex flex-col justify-between">
+    <header class="flex justify-center py-8 w-hull h-48 bg-pink-200 shadow-md">
+        <div class="h-full w-2/4 flex flex-col justify-between">
             <div>
                 <h1 @click="$router.push('/')" class="text-3xl font-bold hover:text-gray-500 cursor-pointer">Pliušinukai</h1>
                 <p>Ranku darbo</p>
             </div>
-            <ul class="flex w-full space-x-6">
-                <li @click="$router.push(item.route)" v-for="(item, label) in list" :class="{'text-gray-500': $route.name == item.name}" class="hover:text-gray-500 cursor-pointer">{{label}}</li>
-            </ul>
+            <nav>
+                <ul class="flex w-full space-x-6">
+                    <li @click="$router.push(item.route)" v-for="(item, label) in list" :class="{'text-gray-500': $route.name == item.name}" class="hover:text-gray-500 cursor-pointer">{{label}}</li>
+                </ul>
+            </nav>
         </div>
-    </nav>
+    </header>
 </template>
