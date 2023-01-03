ae792c5c67382f1cf381b607a6626c0492584f24
boostless <jonas68824@gmail.com>
2023-01-03 20:56:08 +0200

    Update(router,Nav): add routes

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,7 +2,24 @@
 export default {
     data(){
         return {
-            list: ['Naujienos', 'Zaisliukai', 'Apie mus', 'Galerija']
+            list: {
+                'Naujienos': {
+                    route: '/',
+                    name: 'Index'
+                },
+                'Žaisliukai': {
+                    route: '/zaisliukai',
+                    name: 'Zaisliukai'
+                },
+                'Apie mus': {
+                    route: '/apie_mus',
+                    name: 'Apie_mus'
+                },
+                'Kontaktai': {
+                    route: '/kontaktai',
+                    name: 'Kontaktai'
+                }
+            }
         }
     }
 }
@@ -12,11 +29,11 @@ export default {
     <nav class="flex w-full h-48 bg-pink-200 shadow-md">
         <div class="h-full w-2/4 mx-auto pt-8 pb-4 flex flex-col justify-between">
             <div>
-                <h1 class="text-3xl font-bold">Pliušinukai</h1>
+                <h1 @click="$router.push('/')" class="text-3xl font-bold hover:text-gray-500 cursor-pointer">Pliušinukai</h1>
                 <p>Ranku darbo</p>
             </div>
             <ul class="flex w-full space-x-6">
-                <li v-for="item in list" class="hover:text-gray-500 cursor-pointer">{{item}}</li>
+                <li @click="$router.push(item.route)" v-for="(item, label) in list" :class="{'text-gray-500': $route.name == item.name}" class="hover:text-gray-500 cursor-pointer">{{label}}</li>
             </ul>
         </div>
     </nav>
--- src/routes/router.js
+++ src/routes/router.js
@@ -1,11 +1,29 @@
 import { createWebHistory, createRouter } from "vue-router";
 import Index from '../pages/Index.vue'
+import Apie_mus from '../pages/Apie_mus.vue'
+import Kontaktai from '../pages/Kontaktai.vue'
+import Zaisliukai from '../pages/Zaisliukai.vue'
 
 const routes = [
     {
         path: '/',
         name: 'Index',
         component: Index
+    },
+    {
+        path: '/zaisliukai',
+        name: 'Zaisliukai',
+        component: Zaisliukai
+    },
+    {
+        path: '/apie_mus',
+        name: 'Apie mus',
+        component: Apie_mus
+    },
+    {
+        path: '/kontaktai',
+        name: 'Kontaktai',
+        component: Kontaktai
     }
 ]
 
