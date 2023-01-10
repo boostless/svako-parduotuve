63e31d218a959263b3a1ebe78f04f7414f093be2
boostless <jonas68824@gmail.com>
2023-01-10 23:15:00 +0200

    Netlify ?

--- src/pages/Kontaktai.vue
+++ src/pages/Kontaktai.vue
@@ -6,7 +6,7 @@ import PageHeader from '../components/PageHeader.vue';
     <div class="w-full min-h-fit px-32 pb-24">
         <PageHeader title="Susisiekite su mumis" subtitle="Norėtume išgirsti jūsų nuomonę. Užpildykite šią formą." />
         <section class="w-full flex justify-center">
-            <form class="min-h-fit w-2/6 px-6 space-y-6">
+            <form class="min-h-fit w-2/6 px-6 space-y-6" name="contact" method="POST" data-netlify="true">
                 <div class="w-full flex justify-between">
                     <div class="flex flex-col space-y-2">
                         <label for="name" class="text-sm font-medium">Vardas <span class="text-red-500">*</span></label>
@@ -26,14 +26,14 @@ import PageHeader from '../components/PageHeader.vue';
                     <input class="border p-3 rounded-lg" id="phone_number" type="text" placeholder="+370 600 50600">
                 </div>
                 <div class="w-full flex flex-col space-y-2">
-                    <label for="text_area" class="text-sm font-medium">Žinutė <span class="text-red-500">*</span></label>
-                    <textarea required="true" class="border p-3 rounded-lg resize-none h-32" placeholder="Įrašykite žinutę..."></textarea>
+                    <label for="message" class="text-sm font-medium">Žinutė <span class="text-red-500">*</span></label>
+                    <textarea id="message" required="true" class="border p-3 rounded-lg resize-none h-32" placeholder="Įrašykite žinutę..."></textarea>
                 </div>
                 <div class="w-full flex space-x-2 items-center">
-                    <input required="true" class="h-5 w-5 rounded-lg" type="checkbox">
-                    <label for="text_area" class="text-gray-500 font-medium">Sutinkus su <a class="underline cursor-pointer">privatumo politika</a>. <span class="text-red-500">*</span></label>
+                    <input id="privacy" required="true" class="h-5 w-5 rounded-lg" type="checkbox">
+                    <label for="privacy" class="text-gray-500 font-medium">Sutinkus su <a class="underline cursor-pointer">privatumo politika</a>. <span class="text-red-500">*</span></label>
                 </div>
-                <button class="w-full text-center py-3 rounded-lg bg-purple-600 text-white font-semibold">Siųsti žinutę</button>
+                <button type="submit" class="w-full text-center py-3 rounded-lg bg-purple-600 text-white font-semibold">Siųsti žinutę</button>
             </form> 
         </section>   
     </div>
