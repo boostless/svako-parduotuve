73e56f07b18c65d707727de2a93a71ac68883164
boostless <jonas68824@gmail.com>
2023-01-10 22:37:30 +0200

    Update PageHeader.vue

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -14,7 +14,7 @@ const props = defineProps({
 
 <template>
     <header class="py-24">
-        <p v-if="route != 'false'" class="text-center text-purple-600 font-semibold">{{ this.$route.name }}</p>
+        <p v-if="route != 'false'" class="text-center text-purple-600 font-semibold">{{ $route.name }}</p>
         <h1 class="text-center font-semibold text-5xl mt-6">{{ title }}</h1>
         <p class="text-gray-500 text-xl text-center mt-7">{{ subtitle }}</p>
     </header>
