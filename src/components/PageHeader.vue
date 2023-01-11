5d95c3451cea906e4fe51d91a20a297d13683d78
boostless <jonas68824@gmail.com>
2023-01-11 11:40:47 +0200

    update(PageHeader): responsive

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -15,7 +15,7 @@ const props = defineProps({
 <template>
     <header class="py-24">
         <p v-if="route != 'false'" class="text-center text-purple-600 font-semibold">{{ $route.name }}</p>
-        <h1 class="text-center font-semibold text-5xl mt-6">{{ title }}</h1>
-        <p class="text-gray-500 text-xl text-center mt-7 break-words">{{ subtitle }}</p>
+        <h1 class="text-center font-semibold text-lg md:text-xl lg:text-5xl mt-2 md:mt-6">{{ title }}</h1>
+        <p class="text-gray-500 text-xs sm:text-base md:text-xl text-center mt-3 md:mt-7 break-words">{{ subtitle }}</p>
     </header>
 </template>
