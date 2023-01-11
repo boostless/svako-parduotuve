d85a7a3a86c7d8ebbac63cda225be4eea67f2c93
boostless <jonas68824@gmail.com>
2023-01-11 11:35:38 +0200

    update(Zaisliukai): responsive grid

--- src/pages/Zaisliukai.vue
+++ src/pages/Zaisliukai.vue
@@ -4,9 +4,9 @@ import ShopPost from '../components/ShopPost.vue';
 </script>
 
 <template>
-    <div class="w-full min-h-fit px-32">
+    <div class="w-full min-h-fit px-32 pb-24">
         <PageHeader title="Parduotuvė" subtitle="Išsirinkite jums patinkantį žaisliuką ir mes jį pristatysime kuo greičiau" />
-        <div class="w-full grid grid-cols-5 auto-rows-auto place-items-center gap-16">
+        <div class="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 auto-rows-auto place-items-center gap-16">
             <ShopPost v-for="i in 20" title="Meskiukas" price="25,00" size="M"/>
         </div>
     </div>
