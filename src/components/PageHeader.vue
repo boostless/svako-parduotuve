5c992d34d99e1ae429666accebeb6f9b6101857e
boostless <jonas68824@gmail.com>
2023-01-11 11:42:07 +0200

    ref(PageHeader): responsive padding

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -13,9 +13,9 @@ const props = defineProps({
 </script>
 
 <template>
-    <header class="py-24">
+    <header class="py-10 sm:py-24">
         <p v-if="route != 'false'" class="text-center text-purple-600 font-semibold">{{ $route.name }}</p>
         <h1 class="text-center font-semibold text-lg md:text-xl lg:text-5xl mt-2 md:mt-6">{{ title }}</h1>
-        <p class="text-gray-500 text-xs sm:text-base md:text-xl text-center mt-3 md:mt-7 break-words">{{ subtitle }}</p>
+        <p class="text-gray-500 text-sm sm:text-base md:text-xl text-center mt-3 md:mt-7 break-words">{{ subtitle }}</p>
     </header>
 </template>
