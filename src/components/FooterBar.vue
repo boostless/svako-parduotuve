7bad559579ae6d01cad4eaec280260823d57d0b7
boostless <jonas68824@gmail.com>
2023-01-15 12:48:42 +0200

    update(Footer): responsive

--- src/components/FooterBar.vue
+++ src/components/FooterBar.vue
@@ -1,6 +1,6 @@
 <template>
-    <footer class="flex flex-col bg-purple-950 text-white px-32">
-        <div class="w-full flex items-center justify-between py-16">
+    <footer class="flex flex-col bg-purple-950 text-white px-32 mt-auto">
+        <div class="w-full hidden md:flex items-center justify-between py-16">
             <header class="space-y-2">
                 <h3 class="text-xl font-medium">Užsiprenumeruoktie mūsų naujienlaiškį</h3>
                 <p class="text-gray-200 font-normal">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum.</p>
@@ -14,9 +14,9 @@
             </div>
         </div>
         <div class="w-full h-px bg-purple-450"></div>
-        <div class="w-full flex items-center justify-between py-12">
+        <div class="w-full flex flex-col space-y-2 sm:space-y-0 sm:flex-row items-center justify-center sm:justify-between py-12">
             <span class="text-xl font-semibold">Pliušinukai</span>
-            <span class="text-sm text-purple-10">© 2023 Pliušinukai. Visos teisės saugomos.</span>
+            <span class="text-sm text-purple-10 text-center">© 2023 Pliušinukai. Visos teisės saugomos.</span>
         </div>
     </footer>
 </template>
